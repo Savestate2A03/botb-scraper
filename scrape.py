44d8ba1f66b151eb9f9a09c97f63f45e9913b483
Joseph El-Khouri <joseph.elkhouri+github@gmail.com>
2017-01-28 20:43:56 -0500

    use http.client instead, logging in works
    get php session id 1st, then login

--- scrape.py
+++ scrape.py
@@ -1,26 +1,56 @@
-import urllib.request
+import http.client
 import getpass
 
+def default_headers(client):
+    client.putheader('Accept', 'text/html')
+    client.putheader('Accept-Language', 'en-US')
+    client.putheader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36')
+
+base_url = 'battleofthebits.org'
+base_dir = '/arena/Entry/botb-scraper/'
+base_login = '/barracks/Login/'
+req_boundary = '----BotBScraperBoundary'
+
 # Get session cookies which allows for downloading BotB content
-print('Please provide your BotB session cookies!')
-botb_cookies = {}
-botb_cookie_types = ['PHPSESSID', 'botbr_id', 'serial', 'user_id']
-for botb_cookie_type in botb_cookie_types:
-    print(botb_cookie_type)
-    botb_cookies[botb_cookie_type] = getpass.getpass(" --> ")
-print(botb_cookies)
+print('Please sign into BotB')
+botb_user = input(' emailadd: ')
+botb_pass = getpass.getpass(' password: ')
 
-# http://battleofthebits.org/arena/Entry/botb-scraper/X/
-# where X is the entry ID
+# Get PHP SessionID
+client = http.client.HTTPConnection(base_url)
+client.connect()
+client.putrequest("GET", base_dir)
+default_headers(client)
+client.endheaders()
+response = client.getresponse()
+# Get cookie through black magic
+php_session = [header[1] for header in response.getheaders() if header[0] == 'Set-Cookie'][0]
+client.close()
 
-# if it contains ...
-# <a href="/barracks/Signup/" title="register">
-# you are currently not logged in (cookies failed!)
+# Sign into BotB using the provided credidentials
+client.putrequest("POST", base_login)
+default_headers(client)
+client.putheader('Content-Type', 'multipart/form-data; boundary=' + req_boundary)
+client.putheader('Cookie', php_session)
 
-signup_text = '<a href="/barracks/Signup/" title="register">'
-with urllib.request.urlopen('http://battleofthebits.org/arena/Entry/botb-scraper/1') as response:
-    html = str(response.read())
-    if signup_text in html:
-        print('Invalid Cookies (not signed in)')
-    else:
-        print('Valid Cookies (signed in)')
+# Build the request payload
+payload = '--' + req_boundary + '\n'
+payload += 'Content-Disposition: form-data; name="email"\n'
+payload += '\n'
+payload += botb_user + '\n'
+payload += '--' + req_boundary + '\n'
+payload += 'Content-Disposition: form-data; name="password"\n'
+payload += '\n'
+payload += botb_pass + '\n'
+payload += '--' + req_boundary + '\n'
+payload += 'Content-Disposition: form-data; name="submitok"\n'
+payload += '\n'
+payload += 'LOGIN\n'
+payload += '--' + req_boundary
+client.putheader('Content-Length', len(payload))
+client.endheaders()
+
+# Send the payload
+client.send(payload.encode())
+response = client.getresponse()
+
