7b8bfb745cf7bb761eaa1ed2e68f3283267523b6
Joseph El-Khouri <joseph.elkhouri+github@gmail.com>
2017-02-13 21:03:52 -0500

    the terminal can't unicode! :calling:

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -5,6 +5,7 @@ import urllib
 
 import base64
 import os
+import sys
 
 from pathlib import Path
 import errno
@@ -302,10 +303,10 @@ def download_entry(botb_cookies, entry_number, input_subfolder):
     if len(filename) > 70:
         filename_r = re.compile(r'^(.{1,70}).*\.(\S+)')
         filename_m = filename_r.match(filename)
-        print(filename)
+        print(filename.encode(sys.stdout.encoding, errors='replace').decode('utf-8'))
         print(' ! - has been shortened to - ! ')
         filename = filename_m.group(1) + '.' + filename_m.group(2)
-        print(filename)
+        print(filename.encode(sys.stdout.encoding, errors='replace').decode('utf-8'))
     f = open('files/' + input_subfolder + '/orig/' + filename, 'wb')
     f.write(original_file)
     f.close()
